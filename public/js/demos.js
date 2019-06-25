92a5b04b78c69f8a23a3eb0656574a0d7717afa3
stephenjk41 <stephenjk41@gmail.com>
2019-06-24 23:07:11 -0400

    tests for demos

---
+++ public/js/demos.js
@@ -0,0 +1,8 @@
+firebase.initializeApp({
+    apiKey: 'AIzaSyDmsh8zsA5xwFJ5XVHTK-TitagdlepZ2Hk',
+    authDomain: '"corboration-265.firebaseapp.com"',
+    projectId: 'corboration-265',
+    storageBucket: "corboration-265.appspot.com",
+    databaseURL: "https://corboration-265.firebaseio.com",
+});
+
