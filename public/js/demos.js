eae5c8d0b47becd8fdb36f553d92d9ea72f23c72
stephenjk41 <stephenjk41@gmail.com>
2019-06-26 07:58:12 -0400

    demo update

--- public/js/demos.js
+++ public/js/demos.js
@@ -1,8 +1,9 @@
-firebase.initializeApp({
-    apiKey: 'AIzaSyDmsh8zsA5xwFJ5XVHTK-TitagdlepZ2Hk',
-    authDomain: '"corboration-265.firebaseapp.com"',
-    projectId: 'corboration-265',
-    storageBucket: "corboration-265.appspot.com",
-    databaseURL: "https://corboration-265.firebaseio.com",
-});
+// firebase.initializeApp({
+//     apiKey: 'AIzaSyDmsh8zsA5xwFJ5XVHTK-TitagdlepZ2Hk',
+//     authDomain: '"corboration-265.firebaseapp.com"',
+//     projectId: 'corboration-265',
+//     storageBucket: "corboration-265.appspot.com",
+//     databaseURL: "https://corboration-265.firebaseio.com",
+// });
+
 
